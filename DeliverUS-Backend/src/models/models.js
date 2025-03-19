875e084ea790c07bbf92b3b02584a46ed80ada7b
fabrobher <fabrobher@alum.us.es>
2025-03-19 21:21:07 +0100

    preparacion

--- DeliverUS-Backend/src/models/models.js
+++ DeliverUS-Backend/src/models/models.js
@@ -6,6 +6,8 @@ import loadProductModel from './Product.js'
 import loadProductCategoryModel from './ProductCategory.js'
 import loadRestaurantCategoryModel from './RestaurantCategory.js'
 import loadUserModel from './User.js'
+// Needed for statement
+import loadPerformanceModel from './Performance.js'
 
 const sequelizeSession = new Sequelize(getEnvironmentConfig().database, getEnvironmentConfig().username, getEnvironmentConfig().password, getEnvironmentConfig())
 const Restaurant = loadRestaurantModel(sequelizeSession, Sequelize.DataTypes)
@@ -14,8 +16,11 @@ const Product = loadProductModel(sequelizeSession, Sequelize.DataTypes)
 const ProductCategory = loadProductCategoryModel(sequelizeSession, Sequelize.DataTypes)
 const RestaurantCategory = loadRestaurantCategoryModel(sequelizeSession, Sequelize.DataTypes)
 const User = loadUserModel(sequelizeSession, Sequelize.DataTypes)
+// Needed for statement
+const Performance = loadPerformanceModel(sequelizeSession, Sequelize.DataTypes)
 
-const db = { Restaurant, Order, Product, ProductCategory, RestaurantCategory, User }
+// Needed for statement
+const db = { Restaurant, Order, Product, ProductCategory, RestaurantCategory, User, Performance }
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
@@ -23,4 +28,5 @@ Object.keys(db).forEach(modelName => {
   }
 })
 
-export { Restaurant, Order, Product, ProductCategory, RestaurantCategory, User, sequelizeSession }
+// Needed for statement
+export { Performance, Restaurant, Order, Product, ProductCategory, RestaurantCategory, User, sequelizeSession }
